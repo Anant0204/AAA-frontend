7dfad2619c4ddd14abd3ec1cf748d68920cb3d2e
krishnarajput <161302830+KrishnpalJadam@users.noreply.github.com>
2026-07-24 19:04:45 +0530

    Import dayjs in SuperAdminAgents.jsx

--- src/pages/team/SuperAdminAgents.jsx
+++ src/pages/team/SuperAdminAgents.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import dayjs from 'dayjs';
 
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import Box from '@mui/material/Box';
